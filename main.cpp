14aad81a162a339858c24bb24c085e825b1dd87e
Crystalpiny <805447090@qq.com>
2024-03-24 19:02:03 +0800

    1.引入bool变量来标识skiplist的benchmark是否已经进行过；

--- main.cpp
+++ main.cpp
@@ -18,24 +18,35 @@ int main()
 {
     system("chcp 65001");   // 设置字符编码为UTF-8，解决中文乱码问题。
 
-    while (true)  // 使用无限循环来保持程序运行，直到用户选择退出。
+    bool skiplist_benchmark_executed = false; // 标志变量，用于标记Benchmark测试是否已经执行过。
+
+    while (true)
     {
         std::cout << "选择操作：\n1. 进行Benchmark测试\n2. 跳表API接口测试\n3. 其他功能\n4. 退出程序\n请输入选项:" << std::endl;
-        int choice;  // 存储用户的选择。
-        std::cin >> choice;  // 从标准输入读取用户的选择。
+        int choice;
+        std::cin >> choice;
 
-        if (std::cin.fail()) {  // 检查输入是否失败，例如由于输入非数字。
-            std::cin.clear();  // 清除输入流的错误标志。
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // 忽略错误的输入直到下一个换行符。
-            std::cout << "输入无效。请输入数字。\n";  // 提示用户输入有效的数字。
-            continue;  // 跳过循环的剩余部分，重新开始循环。
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "输入无效。请输入数字。\n";
+            continue;
         }
 
-        switch (choice)  // 根据用户的选择执行相应的操作。
+        switch (choice)
         {
             case 1:
-                // 进入Benchmark测试框架。
-                skiplist_benchmark();
+                if (skiplist_benchmark_executed)
+                {
+                    // 如果Benchmark测试已经执行过，提示用户并返回主菜单。
+                    std::cout << "Benchmark测试已经执行过。请选择其他操作。\n";
+                }
+                else
+                {
+                    // 进入Benchmark测试框架，并标记Benchmark测试已执行。
+                    skiplist_benchmark();
+                    skiplist_benchmark_executed = true; // 标记Benchmark测试已经执行过。
+                }
                 break;
             case 2:
                 // 进行跳表API接口测试。
@@ -45,13 +56,11 @@ int main()
                 // 进入其他功能的处理。
                 break;
             case 4:
-                std::cout << "退出程序。" << std::endl;  // 打印退出提示。
-                return 0;  // 返回0，退出程序。
+                std::cout << "退出程序。" << std::endl;
+                return 0;
             default:
-                std::cout << "无效选项。请重新输入。" << std::endl;  // 提示用户重新输入有效选项。
+                std::cout << "无效选项。请重新输入。\n";
         }
     }
-    
-    // 实际上，这里的 return 0 是多余的，因为程序会在 case 3 中通过 return 0 退出。
     return 0;
 }
