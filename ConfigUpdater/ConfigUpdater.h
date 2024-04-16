d404fdba3f8ee45ddeff483c1a60f177cde93dcc
Crystalpiny <805447090@qq.com>
2024-04-16 19:04:34 +0800

    1.完善配置文件写入方法的注释模版；

--- ConfigUpdater/ConfigUpdater.h
+++ ConfigUpdater/ConfigUpdater.h
@@ -50,7 +50,40 @@ public:
     static bool UpdateUseRandRNG(const std::string &filename, bool newValue);
 };
 
+/**
+ * @brief 提示用户做出是或否的选择。
+ *
+ * @param prompt 显示给用户的提示字符串。
+ * @return 用户输入的布尔选择。
+ * @details 这个函数不断地提示用户输入，直到得到有效的输入（1或0）为止。
+ *          如果用户输入无效，它会清除输入流并提示用户重新输入。
+ * @note 输入被限制为整数1或0，其中1代表是，0代表否。
+ */
 bool promptForBoolean(const std::string &prompt);
+
+/**
+ * @brief 更新配置文件中的特定字段。
+ *
+ * @param configFilePath 配置文件的路径。
+ * @param section 配置文件中要更新的部分。
+ * @param field 要更新的字段。
+ * @param value 要设置的新布尔值。
+ * @return 更新成功返回true，否则返回false。
+ * @details 函数首先尝试读取配置文件，并解析为JSON对象。
+ *          然后，它会检查指定的部分并更新字段的值。
+ *          最后，它会将更新后的JSON对象写回配置文件。
+ * @note 使用了RapidJSON库来处理JSON数据。
+ *       需要确保配置文件的路径是正确的，并且文件具有正确的读写权限。
+ */
 bool updateConfigFile(const std::string &configFilePath, const std::string &section, const std::string &field, bool value);
+
+/**
+ * @brief 主函数，处理更新配置的逻辑。
+ *
+ * @details 该函数提示用户选择要更新的配置字段。
+ *          根据用户的选择，执行相应的更新逻辑。
+ * @note 该函数依赖于提供的配置选项映射。
+ *       映射中每个选项都有一个对应的lambda函数，用于处理用户的选择。
+ */
 void updateConfiguration();
 #endif // CONFIGUPDATER_H
